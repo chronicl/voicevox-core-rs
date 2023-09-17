6c838b76fe5d60bdb83e729d77209533d996d750
Lennard Koehler <lennard.koehler@gmx.net>
2023-09-17 14:22:14 +0200

    .

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("cargo:rustc-link-search=native=voicevox_core");
-    println!("cargo:rustc-link-lib=static:+whole-archive,-bundle=voicevox_core");
+    println!("cargo:rustc-link-lib=static=voicevox_core");
 }
